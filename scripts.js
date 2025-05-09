6ce5fdc0381b29da6db2793ff0dd087c0228642d
Gabriel Santana <81117392+biel-goncalves@users.noreply.github.com>
2025-05-09 20:20:46 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Este arquivo pode ser utilizado para adicionar interatividade mais tarde
+console.log("Site carregado!");
+console.log("Site carregado!");
+console.log("Site carregado!");
+console.log("Site carregado!");
+console.log("Site carregado!");
+console.log("Site carregado!");
+console.log("Site carregado!");
